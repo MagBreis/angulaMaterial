56c6c2110ac313f8f9c13eefbcd64de56e89a3b1
Maga <87536225+MagBreis@users.noreply.github.com>
2021-10-13 23:44:32 -0300

    angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatCardModule,
+
   ],
-  providers: [],
+  providers: [
+
+  ],
+  exports:[
+    MatCardHarness,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
